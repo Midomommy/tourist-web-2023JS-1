a8dbdc95a00c2d285b4f6fc44c4fd23d1f23bd39
Midomommy <tinasue165@gmail.com>
2023-10-24 02:54:27 +0800

    update version2

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const url = "https://tourist-web-2023js-1.onrender.com";
-// let id = "";
-// const apiUrl = 'http://localhost:3000' || 'https://tourist-web-2023js-1.onrender.com';
-let token = localStorage.getItem("token");
-
-// token = localStorage.getItem("token")
